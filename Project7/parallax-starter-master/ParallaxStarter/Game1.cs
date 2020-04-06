b719a21075f96a2ae3b2fc500101d55430e1d367
ramos04 <ramos04@ksu.edu>
2020-04-05 22:13:16 -0500

    finished project7

---
+++ Project7/parallax-starter-master/ParallaxStarter/Game1.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace ParallaxStarter
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Player player;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            // PLAYER
+            //-----------------------------------------------------------
+            var spritesheet = Content.Load<Texture2D>("player");
+            player = new Player(spritesheet, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            var playerLayer = new ParallaxLayer(this);
+            playerLayer.ScrollController = new PlayerTrackingScrollController(player, 1.0f);
+            playerLayer.Sprites.Add(player);
+            playerLayer.DrawOrder = 5;
+            Components.Add(playerLayer);
+
+            // BACKGROUND
+            //-----------------------------------------------------------
+            var backgroundTexture = Content.Load<Texture2D>("finalNight");
+            var backgroundSprite = new StaticSprite(backgroundTexture);
+            var backgroundLayer = new ParallaxLayer(this);
+            backgroundLayer.ScrollController = new PlayerTrackingScrollController(player, 0.1f);
+            backgroundLayer.Sprites.Add(backgroundSprite);
+            backgroundLayer.DrawOrder = 0;
+            Components.Add(backgroundLayer);
+
+
+            var midgroundTexture = Content.Load<Texture2D>("sky");
+            var midgroundSprite = new StaticSprite(midgroundTexture);
+            var midgroundLayer = new ParallaxLayer(this);
+            midgroundLayer.ScrollController = new PlayerTrackingScrollController(player, 0.2f);
+            midgroundLayer.Sprites.Add(midgroundSprite);
+            midgroundLayer.DrawOrder = 1;
+            Components.Add(midgroundLayer);
+
+            // Foreground
+            //-----------------------------------------------------------
+            var foregroundTexture = new Texture2D[] {
+                Content.Load<Texture2D>("trees1"),
+                Content.Load<Texture2D>("trees1")
+            };
+
+            var foregroundSprite = new StaticSprite[]
+            {
+                new StaticSprite(foregroundTexture[0]),
+                new StaticSprite(foregroundTexture[1], new Vector2(3500, 0))
+            };
+
+            var foregroundLayer = new ParallaxLayer(this);
+            foregroundLayer.ScrollController = new PlayerTrackingScrollController(player, 0.5f);
+
+            foregroundLayer.Sprites.AddRange(foregroundSprite);
+            foregroundLayer.DrawOrder = 2;
+ 
+            Components.Add(foregroundLayer);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            player.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            //player.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Project7/parallax-starter-master/ParallaxStarter/Player.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace ParallaxStarter
+{
+    enum HorizontalState
+    {
+        Idle,
+        Left,
+        Right
+    }
+
+    enum VerticalState
+    {
+        Ground,
+        Jumping,
+        Falling
+    }
+
+    public class Player : ISprite
+    {
+
+        Game1 game;                             // The game object
+        /// <summary>
+        /// A spritesheet containing a helicopter image
+        /// </summary>
+        Texture2D spritesheet;
+        TimeSpan jumpTimer;
+        const int JUMP_TIME = 150;
+
+
+        /// <summary>
+        /// The portion of the spritesheet that is the helicopter
+        /// </summary>
+        Rectangle sourceRect = new Rectangle
+        {
+            X = 0,
+            Y = 0,
+            Width = 49,
+            Height = 60
+        };
+
+        private float speed = (float)0.30;      // Speed multiplier 
+        private float vSpeed = (float)1.0;      // Speed multiplier
+        VerticalState verticalState = VerticalState.Ground;
+        HorizontalState horizontalState = HorizontalState.Idle;
+
+        /// <summary>
+        /// The origin of the helicopter sprite
+        /// </summary>
+        Vector2 origin;
+
+        float jumpHeight;
+
+        /// <summary>
+        /// The angle the helicopter should tilt
+        /// </summary>
+        float angle = 0;
+
+        /// <summary>
+        /// The player's position in the world
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        public Vector2 oldPosition { get; set; }
+
+
+        /// <summary>
+        /// How fast the player moves
+        /// </summary>
+        public float Speed { get; set; } = 100;
+
+        /// <summary>
+        /// Constructs a player
+        /// </summary>
+        /// <param name="spritesheet">The player's spritesheet</param>
+        public Player(Texture2D spritesheet, int viewportWidth, int viewportHeight)
+        {
+            this.spritesheet = spritesheet;
+            origin = new Vector2(-150,15);
+            this.Position = new Vector2(viewportWidth - sourceRect.Width, viewportHeight - sourceRect.Width);
+            this.oldPosition = Position;
+            //this.Position = new Vector2(200, 200);
+            //origin = new Vector2(viewportWidth / 2 - sourceRect.Width / 2, viewportHeight - sourceRect.Height);
+
+        }
+
+        /// <summary>
+        /// Updates the player position based on GamePad or Keyboard input
+        /// </summary>
+        /// <param name="gameTime">The GameTime object</param>
+        public void Update(GameTime gameTime)
+        {
+            Vector2 direction = Vector2.Zero;
+            
+            // Use GamePad for input
+            var gamePad = GamePad.GetState(0);
+
+            // The thumbstick value is a vector2 with X & Y between [-1f and 1f] and 0 if no GamePad is available
+            direction.X = gamePad.ThumbSticks.Left.X;
+
+            // We need to inverty the Y axis
+            direction.Y = -gamePad.ThumbSticks.Left.Y;
+            var keyboard = Keyboard.GetState();
+
+            switch (verticalState)
+            {
+                case VerticalState.Ground:
+                    if (keyboard.IsKeyDown(Keys.Space))
+                    {
+                        verticalState = VerticalState.Jumping;
+                        jumpTimer = new TimeSpan(0);
+                    }
+                    break;
+                case VerticalState.Jumping:
+                    jumpTimer += gameTime.ElapsedGameTime;
+                    direction.Y -= (250 / (float)jumpTimer.TotalMilliseconds);
+                    jumpHeight = (250 / (float)jumpTimer.TotalMilliseconds);
+                    if (jumpTimer.TotalMilliseconds >= JUMP_TIME) verticalState = VerticalState.Falling;
+                    break;
+                case VerticalState.Falling:
+                    direction.Y += jumpHeight;
+                    break;
+            }
+
+            // Override with keyboard input
+            if(keyboard.IsKeyDown(Keys.Left) || keyboard.IsKeyDown(Keys.A))
+            {
+                direction.X -= 1;
+            }
+            if (keyboard.IsKeyDown(Keys.Right) || keyboard.IsKeyDown(Keys.D)) 
+            {
+                direction.X += 1;
+            }
+            //if(keyboard.IsKeyDown(Keys.Up) || keyboard.IsKeyDown(Keys.W))
+           // {
+            //    direction.Y -= 1;
+           // }
+            //if(keyboard.IsKeyDown(Keys.Down) || keyboard.IsKeyDown(Keys.S))
+           // {
+           //     direction.Y += 1;
+           // }
+
+
+            angle = 0;
+ 
+            Position += (float)gameTime.ElapsedGameTime.TotalSeconds * Speed * direction;
+
+            if(Position.Y > origin.Y && verticalState == VerticalState.Falling)
+            {
+                Position = new Vector2(Position.X, oldPosition.Y);
+                verticalState = VerticalState.Ground;
+            }
+        }
+
+        /// <summary>
+        /// Draws the player sprite
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            // Render the helicopter, rotating about the rotors
+            spriteBatch.Draw(spritesheet, Position, sourceRect, Color.White, angle, origin, 1f, SpriteEffects.None, 0.7f);
+        }
+
+    }
+}
